90700294108872ff4501b959eaf6ef8ec6945290
N.T.WORKS <nagisc007@yahoo.co.jp>
2020-09-19 20:28:27 +0900

    Add story memo (#3)
    * Add struct
    
    * Add memo
    
    * Add plot memo
    
    * Update v0.2.0 [ci skip]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [0.2.0] - 2020-09-19
+### Added
+- story memo
+
 ## [0.1.0] - 2020-09-16
 ### Added
 - plan
@@ -15,6 +19,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - This CHANGELOG file to hopefully serve as an evolving example of a standardized open source project CHANGELOG.
 - README one line implemented.
 
-[Unreleased]: https://github.com/My-Novel-Management/m133-tears-reason/compare/v0.1.0...HEAD
+[Unreleased]: https://github.com/My-Novel-Management/m133-tears-reason/compare/v0.2.0...HEAD
+[0.2.0]: https://github.com/My-Novel-Management/m133-tears-reason/releases/v0.2.0
 [0.1.0]: https://github.com/My-Novel-Management/m133-tears-reason/releases/v0.1.0
 [0.0.1]: https://github.com/My-Novel-Management/m133-tears-reason/releases/v0.0.1
--- src/config.py
+++ src/config.py
@@ -7,10 +7,20 @@ Story Config
 ASSET = {
         "PERSONS": (
             # (tag / name / full / age (birth) / job / call / info)
-            ('taro', '太郎', '', 17,(1,1), 'male', '学生'),
+            ("yuri", "ユリ", "シラサギ,ユリ", 32,(1,1), "female", "会社員", "me:わたし"),
+            ('misa', 'ミサ', 'アイジョウ,ミサ', 37,(1,1), 'female', '研究員', "me:私"),
+            ("yukio", "ユキオ", "マキタ,ユキオ", 25,(1,1), "male", "助手", "me:僕"),
+            ("akio", "アキオ", "イルマ,アキオ", 40,(1,1), "male", "会社員", "me:俺"),
             ),
         "STAGES": (
             # (tag / name / parent / (geometry) / info)
+            ("Aichi", "愛知県", "Japan"),
+            ("Nagoya", "名古屋市", "Aichi"),
+            ("Station", "駅", "Nagoya"),
+            ("Subway", "地下鉄", "Nagoya"),
+            ("Cinema", "映画館", "Nagoya"),# 三越映画劇場モデル
+            ("Manshion", "マンション", "Nagoya"),
+            ("Labo", "研究所", "Nagoya"),
             ),
         "DAYS": (
             # (tag / name / month / day / year)
